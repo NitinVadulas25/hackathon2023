09b983cb3d02b07e019c3767303cb2bc26b828a0
shane nguyen <shanehnguyen@gmail.com>
2023-04-16 07:41:08 -0700

    final commit

--- static/script.js
+++
@@ -1,6 +0,0 @@
-let fd= document.getElementById("volunteer-form")
-fd.addEventListener("submit", (e) => {
-    e.preventDefault();
-    alert("hello")
-    
-})
--- views.py
+++ views.py
@@ -3,15 +3,13 @@ from flask import Blueprint, render_template, url_for
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=["GET", "POST"])
-def asfhjasfh():
+def home():
     return render_template("home.html")
 
-
 @views.route('/volunteer', methods=["GET", "POST"])
-def hfhs():
+def volunteer():
     return render_template("volunteer.html")
 
-
-@views.route('/create-volunteer', methods=["GET", "POST"])
-def jack():
-    return render_template("create-volunteer.html")
+@views.route('/contributions', methods=["GET", "POST"])
+def reducer():
+    return render_template("reducer.html")
