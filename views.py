10ff8e23963e39b818988d5961773aa6abd88548
shane nguyen <shanehnguyen@gmail.com>
2023-04-15 22:39:09 -0700

    divs

--- views.py
+++ views.py
@@ -3,5 +3,10 @@ from flask import Blueprint, render_template
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=["GET", "POST"])
-def hello():
+def asfhjasfh():
     return render_template("home.html")
+
+
+@views.route('/goober', methods=["GET", "POST"])
+def hfhs():
+    return render_template("base2.html")
